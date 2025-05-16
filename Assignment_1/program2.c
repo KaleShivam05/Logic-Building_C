d9ddf404f9d85a19a2afac425aa9605d597d4ab4
KaleShivam05 <shivamkale0510@gmail.com>
2025-05-17 00:00:28 +0530

    Add files via upload

---
+++ Assignment_1/program2.c
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////////
+//
+// Name :          program2.c
+// Discription :   Display 5 times "Jay Ganesh"
+// Input :         integer
+// Output :        integer , String
+// Author :        Shivam Rajendra Kale
+// Date :          11/05/2025
+//
+/////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("Jay Ganesh.....\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ Assignment_1/program4.c
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////
+//
+// Name :          program4.c
+// Discription :   Chack whether input is divisible 5 or not
+// Input :         integer
+// Output :        integer , String
+// Author :        Shivam Rajendra Kale
+// Date :          11/05/2025
+//
+/////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept input from user
+          if input is divisible 5 then
+            Display Divisible by 5
+          if input is not Divisible 5 then
+            Display not Divisible by 5  
+       
+    Stop         
+*/
+
+#include<stdio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE -1
+
+int Check(int iNo)
+{
+    if((iNo % 5) == 0)
+    {
+        return TRUE;
+    }
+    else 
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number :");
+    scanf("%d",&iValue);
+
+    bRet = Check(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("\n Divisible by 5");
+    }
+    else
+    {
+        printf("Not Divisible by 5");
+    }
+
+    return 0;
+}
