c43b24a40d7a6fed80d5992b455e8ab353074c0e
KaleShivam05 <shivamkale0510@gmail.com>
2025-05-16 23:27:27 +0530

    Add files via upload

---
+++ Assignment_3/program4.c
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////
+//
+// Name :          problem1.c
+// Discription :   Convert case of character
+// Input :         integer
+// Output :        charecter , String
+// Author :        Shivam Rajendra Kale
+// Date :          13/05/2025
+//
+/////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept Charecter from User
+            if Charecter is capital then
+               Convert into Small letter and Display
+    Stop         
+*/
+
+#include<stdio.h>
+
+void DisplayConvert(char CValue)
+{
+    if((CValue >= 'a') && (CValue <= 'z'))
+    {
+        printf("%c",CValue - 32);
+    }
+    else if((CValue >= 'A') && (CValue <= 'Z'))
+    {
+        printf("%c",CValue + 32);
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter character : ");
+    scanf("%c", &cValue);
+
+    DisplayConvert(cValue);
+
+    return 0;
+}
