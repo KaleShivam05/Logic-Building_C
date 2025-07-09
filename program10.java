cfb051c0bfa2da0ed1be6413f37a02efa83e8289
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-09 19:10:41 +0530

    Add files via upload

---
+++ program10.java
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program10.java
+//  Descreption :   Display Even numbers
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Shivam Rajendra Kale
+//  Date :          09/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept number from user
+            return Even numbers
+    Stop
+*/
+
+import java.util.*;
+
+class Number
+{
+    public void DisplayFactors(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= (iNo / 2); iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
+
+class program60
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+        nobj.DisplayFactors(iValue);
+    }
+}
---
+++ program9.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program9.java
+//  Descreption :   Check Even
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Shivam Rajendra Kale
+//  Date :          09/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept number from user
+            Check
+                if number is Even than 
+                    Display number is Even
+                if number is not Even than
+                    Display number is not Even    
+    Stop
+*/
+
+import java.util.*;
+
+class Number
+{
+    public boolean CheckEven(int iNo)
+    {
+        if((iNo % 2) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program59
+{
+    public static void main(String A[])
+    {
+        boolean bRet = false;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int iValue = sobj.nextInt();
+
+        Number nobj = new Number();
+
+        bRet = nobj.CheckEven(iValue);
+
+        if(bRet == true)
+        {
+            System.out.println(iValue+" is even");
+        }
+        else
+        {
+            System.out.println(iValue+" is odd");
+        }
+    }
+}
