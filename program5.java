ceb679b9fa5b9cafc01e550484166d1754fb1874
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-09 19:10:19 +0530

    Add files via upload

---
+++ program5.java
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program5.java
+//  Descreption :   Display addition of two numbers
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Shivam Rajendra Kale
+//  Date :          09/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept number from user
+            return addition of two number
+    Stop
+*/
+
+import java.util.*;
+
+class program55
+{
+    public static int Addition(int iNo1, int iNo2)
+    {
+        int iAns = 0;
+        iAns = iNo1 + iNo2;
+        return iAns;
+    }
+
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0, iRet = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter second number : ");
+        iValue2 = sobj.nextInt();
+
+        iRet = Addition(iValue1, iValue2);
+
+        System.out.println("Addition is : "+iRet);
+
+    }
+}
---
+++ program7.java
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program7.java
+//  Descreption :   Display addition of two numbers
+//  Input :         Integer
+//  Output :        Integer
+//  Author :        Shivam Rajendra Kale
+//  Date :          09/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+/*
+    Start
+        Accept number from user
+            return addition two number
+    Stop
+*/
+
+import java.util.*;
+
+class Arithematic
+{
+    public int Addition(int iNo1, int iNo2)
+    {
+        int iAns = 0;
+        iAns = iNo1 + iNo2;
+        return iAns;
+    }
+}
+
+class program57
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0, iRet = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter second number : ");
+        iValue2 = sobj.nextInt();
+
+        Arithematic aobj = new Arithematic();
+
+        iRet = aobj.Addition(iValue1, iValue2);
+
+        System.out.println("Addition is : "+iRet);
+
+    }
+}
