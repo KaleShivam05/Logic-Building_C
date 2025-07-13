c403829213f8f1aeefc447a7f4e839f7e25f6125
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-13 22:02:27 +0530

    Add files via upload

---
+++ program41.java
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program41.java
+//  Descreption :   Display pattern
+//  Author :        Shivam Rajendra Kale
+//  Date :          11/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+// Input : 4  4
+// Output :   
+/*
+    *   
+    *   *   
+    *   *   *   
+    *   *   *   * 
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        for(i = 1 ; i <= iRow; i++)
+        {
+            for(j = 1;j <= iCol ; j++)
+            {
+                if((i + j) > iRow)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("o\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program41
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
---
+++ program44.java
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program44.java
+//  Descreption :   Accept input String
+//  Input :         String
+//  Output :        String
+//  Author :        Shivam Rajendra Kale
+//  Date :          12/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+
+import java.util.*;
+
+class program44
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        System.out.println("Your name is : "+name);
+    }
+}
