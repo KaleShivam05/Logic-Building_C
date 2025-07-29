a945b603430644fd8a000a4d4312dd1371a35782
KaleShivam05 <shivamkale0510@gmail.com>
2025-07-29 23:55:51 +0530

    Add files via upload

---
+++ program189.java
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program189.java
+//  Descreption :   Create a Linked list
+//  Author :        Shivam Rajendra Kale
+//  Date :          28/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+// Singly Linear Linked List
+
+// | 11 |->| 21 |->| 51 |->| 101 |-> 
+
+
+class node
+{
+    public int no;
+    public node next;
+}
+
+class SinglyLL
+{
+    private node first;
+    private int iCount;
+
+    public SinglyLL()
+    {
+        System.out.println("Inside constructor");
+        first = null;
+        iCount = 0;
+    }
+}
+
+class program372
+{
+    public static void main(String A[])
+    {
+        SinglyLL sobj = new SinglyLL();
+
+    }
+}
