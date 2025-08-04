2abebcbeed797361c9371991f6188cfea065ee99
KaleShivam05 <shivamkale0510@gmail.com>
2025-08-04 22:58:34 +0530

    Add files via upload

---
+++ program210.c
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program210.c
+//  Descreption :   Display 5 times jay ganesh on screen
+//  Author :        Shivam Rajendra Kale
+//  Date :          31/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ program211.c
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program207.c
+//  Descreption :   Use Recursion 
+//  Author :        Shivam Rajendra Kale
+//  Date :          31/07/2025
+//
+//////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+
+    printf("Jay Ganesh...\n");
+
+    Display();
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
