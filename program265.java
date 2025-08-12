71b05f745a594b93b288637a020e32fac3befe33
KaleShivam05 <shivamkale0510@gmail.com>
2025-08-12 23:39:09 +0530

    Add files via upload

---
+++ program265.java
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program265.java
+//  Descreption :   Check file is exits or not
+//  Author :        Shivam Rajendra Kale
+//  Date :          11/08/2025
+//
+//////////////////////////////////////////////////////////////////
+
+
+import java.util.*;
+import java.io.*;
+
+class program448
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the name of Directory : ");
+            String DirName = sobj.nextLine();
+
+            File fobj = new File(DirName);
+
+            if(fobj.exists())
+            {
+                System.out.println("Directory is present");
+
+                File Arr[] = fobj.listFiles();
+
+                System.out.println("Number of files in the directory are : "+Arr.length);
+            }
+            else
+            {
+                System.out.println("There is no such directory");
+            }
+        }
+        catch(Exception eobj)
+        {}
+    }
+}
