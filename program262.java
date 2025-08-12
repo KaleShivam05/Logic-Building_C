4008a5bea9b4e045de482b0ec05f9a82a301a50f
KaleShivam05 <shivamkale0510@gmail.com>
2025-08-12 23:38:49 +0530

    Add files via upload

---
+++ program262.java
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program262.java
+//  Descreption :   write All data in file
+//  Author :        Shivam Rajendra Kale
+//  Date :          11/08/2025
+//
+//////////////////////////////////////////////////////////////////
+
+
+import java.util.*;
+import java.io.*;
+
+class program445
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the name of source file : ");
+            String Source = sobj.nextLine();
+            
+            System.out.println("Enter the name of destination file : ");
+            String Destination = sobj.nextLine();
+            
+            File fSource = new File(Source);
+            if(fSource.exists() == false)
+            {
+                System.out.println("Source file is not present..");
+                return;
+            }
+
+            File fDestination = new File(Destination);
+            if(fDestination.exists() == true)
+            {
+                System.out.println("Destination file is present..");
+                return;
+            }
+
+            fDestination.createNewFile();
+
+            FileInputStream fiobj = new FileInputStream(fSource);
+            FileOutputStream foobj = new FileOutputStream(fDestination);
+            
+            byte Buffer[] = new byte[1024];
+            int iRet = 0;
+
+            while((fiobj.read(Buffer)) != -1)
+            {
+                foobj.write(Buffer);
+            }
+
+            System.out.println("File copy done...");
+        }
+        catch(Exception eobj)
+        {}
+    }
+}
