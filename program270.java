53f91bc74da7d906644879b1a1cc31c84e3b6732
KaleShivam05 <shivamkale0510@gmail.com>
2025-08-23 18:09:51 +0530

    Add files via upload

---
+++ program270.java
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////
+//
+//  File name :     program270.java
+//  Descreption :   Display size of String 
+//  Author :        Shivam Rajendra Kale
+//  Date :          12/08/2025
+//
+//////////////////////////////////////////////////////////////////
+  
+
+import java.util.*;
+import java.io.*;
+
+class program270
+{
+    public static void main(String A[])
+    {
+        try
+        {
+           String Header = "PPA.txt 30";
+
+           System.out.println("Header size is : "+Header.length());
+        }
+        catch(Exception eobj)
+        {}
+    }
+}
